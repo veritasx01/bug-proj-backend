e728783559e9ac7aeb3e30211636e45f99c373ec
veritas <liorgby2@gmail.com>
2025-10-30 20:43:02 +0200

    add bug access cookies

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { _makeId, bugService } from './bug.service.js';
 
 const app = express();
+
+const corsOptions = {
+  origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
 app.get('/', (req, res) => res.send('Hello there'));
 app.listen(3030, () => console.log('Server ready at port 3030'));
@@ -23,6 +33,27 @@ app.get('/api/bug/save', async (req, res) => {
   }
 });
 
+app.get('/api/bug/:bugId', async (req, res) => {
+  const { bugId } = req.params;
+  let visitCount = req.cookies.visitCount || [];
+  visitCount.push(bugId);
+  // cast to array -> set -> array
+  // to erase duplicates
+  visitCount = [...new Set(visitCount)];
+  res.cookie('visitCount', visitCount, { maxAge: 1000 * 7 });
+  console.log(visitCount.length);
+  if (visitCount.length > 3) {
+    res.status(401).send('Wait for a bit');
+    return;
+  }
+  try {
+    const bug = await bugService.getById(bugId);
+    res.send(bug);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.get('/api/bug', async (req, res) => {
   try {
     const bugs = await bugService.query();
@@ -31,15 +62,7 @@ app.get('/api/bug', async (req, res) => {
     console.log(err);
   }
 });
-app.get('/api/bug/:bugId', async (req, res) => {
-  const { bugId } = req.params;
-  try {
-    const bug = await bugService.getById(bugId);
-    res.send(bug);
-  } catch (err) {
-    console.log(err);
-  }
-});
+
 app.get('/api/bug/:bugId/remove', async (req, res) => {
   const { bugId } = req.params;
   try {
