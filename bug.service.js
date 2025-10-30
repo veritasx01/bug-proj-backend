dfa06bfb043601db626595701cc639030ae7b2f7
veritas <liorgby2@gmail.com>
2025-10-30 16:07:54 +0200

    add bug data

--- bug.service.js
+++ bug.service.js
@@ -1,6 +1,6 @@
-import { storageService } from "./async-storage.service.js";
+import fs from 'fs';
 
-const STORAGE_KEY = "bugDB";
+const STORAGE_FILE = './data/bugs.json';
 
 export const bugService = {
   query,
@@ -9,19 +9,56 @@ export const bugService = {
   remove,
 };
 
+let bugs = _loadBugs();
+
+function _loadBugs() {
+  try {
+    const data = fs.readFileSync(STORAGE_FILE, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    return [];
+  }
+}
+
+function _saveBugs() {
+  fs.writeFileSync(STORAGE_FILE, JSON.stringify(bugs, null, 2));
+}
+
 function query() {
-  return storageService.query(STORAGE_KEY);
+  return Promise.resolve([...bugs]); // return shallow copy
 }
+
 function getById(bugId) {
-  return storageService.get(STORAGE_KEY, bugId);
+  const bug = bugs.find((b) => b._id === bugId);
+  return Promise.resolve(bug);
 }
+
 function remove(bugId) {
-  return storageService.remove(STORAGE_KEY, bugId);
+  bugs = bugs.filter((b) => b._id !== bugId);
+  _saveBugs();
+  return Promise.resolve();
 }
+
 function save(bug) {
   if (bug._id) {
-    return storageService.put(STORAGE_KEY, bug);
+    // update
+    const idx = bugs.findIndex((b) => b._id === bug._id);
+    if (idx === -1) return Promise.resolve(null);
+    bugs[idx] = bug;
   } else {
-    return storageService.post(STORAGE_KEY, bug);
+    // create
+    bug._id = _makeId();
+    bugs.push(bug);
   }
+  _saveBugs();
+  return Promise.resolve(bug);
+}
+
+export function _makeId(length = 8) {
+  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let id = '';
+  while (length--) {
+    id += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return id;
 }
--- server.js
+++ server.js
@@ -1,19 +1,51 @@
-import express from "express";
+import express from 'express';
+import { _makeId, bugService } from './bug.service.js';
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello there"));
-app.listen(3030, () => console.log("Server ready at port 3030"));
+app.get('/', (req, res) => res.send('Hello there'));
+app.listen(3030, () => console.log('Server ready at port 3030'));
 
-app.get("/api/bug", (req, res) => {
-  res.send("bug");
+app.get('/api/bug', async (req, res) => {
+  try {
+    const bugs = await bugService.query();
+    res.send(bugs);
+  } catch (err) {
+    console.log(err);
+  }
 });
-app.get("/api/bug/save", (req, res) => {
-  res.send("save");
+app.get('/api/bug/:bugId', async (req, res) => {
+  const { bugId } = req.params;
+  try {
+    const bug = await bugService.getById(bugId);
+    res.send(bug);
+  } catch (err) {
+    console.log(err);
+  }
 });
-app.get("/api/bug/:bugId", (req, res) => {
-  res.send("bugid");
+app.get('/api/bug/:bugId/remove', async (req, res) => {
+  const { bugId } = req.params;
+  try {
+    await bugService.remove(bugId);
+    res.send('removed');
+  } catch (err) {
+    console.log(err);
+  }
 });
-app.get("/api/bug/:bugId/remove", (req, res) => {
-  res.send("bugid remove");
+
+app.post('/api/bug/save', async (req, res) => {
+  const queryObject = req.query;
+  const incomingBug = {
+    title: queryObject.title,
+    severity: +queryObject.severity,
+    description: queryObject.description,
+    createdAt: new Date(),
+  };
+
+  try {
+    await bugService.save(incomingBug);
+    res.send(incomingBug);
+  } catch (err) {
+    console.log(err);
+  }
 });
